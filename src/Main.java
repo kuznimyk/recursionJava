d565fbb9f8d2da93e85042de8f69ad97e311f66d
kuznimyk <kuznimyk@gmail.com>
2024-04-02 11:24:13 -0600

    header commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+<<<<<<< HEAD
+
+                                                Ý Æ Ã-Ø Œ ¿
+
+Code description:
+     Main goals:  use recursion and interfaces to print out triangles and to move elements in the array.
+     No "for" or "while" keywords are allowed.
+
+Authors:
+    * Mykyta Kuznietsov
+
+Class:
+    * AUCSC 112 LAB 1H04
+
+ID numbers:
+    * 1796900
+
+Date:
+    * April 2nd, 2024
+
+*/
+
 public class Main {
     public static void main(String[] args) {
+
         RecursionSolver rec= new RecursionSolver();
         rec.printTriangles(8);
-        int[] arr = new int[]{1,2,3,4,0,5,6,8};
+        int[] arr = new int[]{0, -3, 5, -2, 0, 7, -8, 4, 0};
         rec.posBeforeNeg(arr);
         for(int i = 0; i < arr.length;i++){
             System.out.print(arr[i]);
